9c254e1de57e42c24f8107bcad2446ed4a20b8c2
unknown <156324875+lalifergui@users.noreply.github.com>
2025-10-15 13:16:28 +0200

    Proyecto Android final con login y registro

---
+++ app/src/main/java/com/example/pantallalogin/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.pantallalogin
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val registro = findViewById<TextView>(R.id.textView2)
+        registro.setOnClickListener {
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
